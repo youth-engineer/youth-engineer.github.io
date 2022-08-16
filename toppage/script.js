4425eaf1e5c945cf7f21f958de28d10169ab4ad3
fukusen <academicforone01@gmail.com>
2022-08-16 18:27:19 +0900

    slickの追加

--- toppage/script.js
+++ toppage/script.js
@@ -1,3 +1,10 @@
 $(function(){
-
+    $('.managerBlock').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        infinite: false
+    })
 });
